13d1d00fc7965dbff09b79ada890b195507b580b
weirdball7 <moteamitay04@gmail.com>
2024-07-21 21:32:10 +0300

    Started working on the project with react not really sure what im doing but everything seems to be working so far

---
+++ Gallerias/src/footer.jsx
@@ -0,0 +1,12 @@
+function Footer(){
+
+    return(
+        <footer>
+            <hr />
+            <p>&copy; {new Date().getFullYear()} Gallerias</p>
+        </footer>
+    );
+
+};
+
+export default Footer;
---
+++ Gallerias/src/mainSection.jsx
@@ -0,0 +1,15 @@
+function Main () {
+    return(
+        <main>
+            <section className="hero-section">
+                <div className="hero">
+                    <h1>Welcome to Gallerias!</h1>
+                    <p>Your middleman for everything art</p>
+                    <button>Sign up now!</button>
+                </div>
+            </section>
+        </main>
+    );
+};
+
+export default Main;
