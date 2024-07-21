b92f2fa2ad1725a2a59b8fb683ea6a2d681d05ba
weirdball7 <moteamitay04@gmail.com>
2024-07-21 21:32:10 +0300

    Started working on the project with react not really sure what im doing but everything seems to be working so far

---
+++ Gallerias/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './header';
+
+import Footer from './footer';
+import Main from './mainSection';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
---
+++ Gallerias/src/header.jsx
@@ -0,0 +1,20 @@
+function Header() {
+
+    return(
+        <header>
+            <h1>Gallerias</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Sign in</a></li>
+                    </ul>
+                
+                </nav>
+                <hr />
+        </header>
+    );
+}
+
+export default Header
