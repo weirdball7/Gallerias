6281f3f647bfc1668936f06288f98080a71711f0
weirdball7 <moteamitay04@gmail.com>
2024-07-24 00:13:10 +0300

    Deployed website

--- Gallerias/vite.config.js
+++ Gallerias/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Gallerias/", // YOUR REPO NAME HERE
+});
