86d771ad0e8a588015c20138b581523715c9c097
weirdball7 <moteamitay04@gmail.com>
2024-07-27 19:46:43 +0300

    added signin form

--- Gallerias/src/App.jsx
+++ Gallerias/src/App.jsx
@@ -6,17 +6,16 @@ import Footer from './footer';
 import Artists from './pages/Artists';
 import SigninSignup from './pages/SigninSignup';
 
-
 function App() {
   return (
-    <Router basename='/Gallerias'>
+    <Router basename="/Gallerias">
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/exhibitions" element={<div>Exhibitions</div>} />
         <Route path="/artists" element={<Artists />} />
         <Route path="/paintings" element={<div>Paintings for Sale</div>} />
-        <Route path="/Signin" element={<SigninSignup />} />
+        <Route path="/signin" element={<SigninSignup />} />
       </Routes>
       <Footer />
     </Router>
