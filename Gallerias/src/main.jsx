3b72216b6b9623758e71d028df5454e8b431b1e1
weirdball7 <moteamitay04@gmail.com>
2024-08-20 23:09:52 +0300

    redesigned and added signin and signup pages

--- Gallerias/src/main.jsx
+++ Gallerias/src/main.jsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css';
-=======
->>>>>>> fd7aacf4c6debc85c53ead39c87c2c15586b10f9
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
